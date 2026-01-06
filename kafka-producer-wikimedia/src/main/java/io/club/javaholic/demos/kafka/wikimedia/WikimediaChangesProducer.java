b8a91cc74413061a5a831cfcdf759a8b643a0f0b
khodyko <idv1llka@gmail.com>
2026-01-06 20:08:22 +0300

    add config for kafka <2.8 and compression

--- kafka-producer-wikimedia/src/main/java/io/club/javaholic/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/club/javaholic/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -5,6 +5,7 @@ import com.launchdarkly.eventsource.EventSource;
 import okhttp3.Headers;
 import okhttp3.internal.http2.Header;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 
@@ -14,6 +15,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
 
 public class WikimediaChangesProducer {
 
@@ -25,9 +28,21 @@ public class WikimediaChangesProducer {
         properties.setProperty("bootstrap.servers", "localhost:9092");
 
 
-        properties.setProperty("key.serializer", StringSerializer.class.getName());
-        properties.setProperty("value.serializer", StringSerializer.class.getName());
-        properties.setProperty("batch.size", "400");
+
+        properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+
+        //set safe producer configs (KAFKA<= 2.8)
+        properties.setProperty(ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ACKS_CONFIG, "all");
+        properties.setProperty(RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+
+        //set high throughput producer config
+        properties.setProperty(LINGER_MS_CONFIG, "20");
+        properties.setProperty(BATCH_SIZE_CONFIG, Integer.toString(32*1024));
+        properties.setProperty(COMPRESSION_TYPE_CONFIG, "snappy");
+
 
         // create Producer
 
